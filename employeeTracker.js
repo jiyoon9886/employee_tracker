5bd07fb78272264260414d1dc233e5c730a028bd
Jiyoon Kim <jiyoon9886@gmail.com>
2020-05-05 02:02:34 -0400

    wrote function to get user input

--- employeeTracker.js
+++ employeeTracker.js
@@ -18,6 +18,62 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
+  askUserChoice();
   });
 
-  
+function askUserChoice() {
+    inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "Add a department",
+        "Add an employee role",
+        "Add employee",
+        "View departments",
+        "View roles",
+        "View employees",
+        "Update employee role",
+        "Exit"
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.action) {
+      case "Add a department":
+        addDepartment();
+        break;
+
+      case "Add an employee role":
+        addRole();
+        break;
+
+      case "Add employee":
+        addEmployee();
+        break;
+
+      case "View departments":
+        viewDepartments();
+        break;
+      
+      case "View roles":
+        viewRoles();
+        break;
+
+      case "View employees":
+        viewEmployees();
+        break;
+
+      case "Update employee role":
+        updateEmployee();
+        break;
+
+      case "exit":
+        connection.end();
+        break;
+      }
+    });
+}
+
+
+
