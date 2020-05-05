8b660b94cbf1bc4356ff51909c5eb9fc5b1d80e0
Jiyoon Kim <jiyoon9886@gmail.com>
2020-05-05 01:14:27 -0400

    created js file and connected to schema.sql

---
+++ employeeTracker.js
@@ -0,0 +1,23 @@
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "jy2052927",
+  database: "employees_DB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  });
+
+  
